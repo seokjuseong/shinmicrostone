f1d5201cb4d6463586386a64b6a3e759d8c09db0
이지은 <lje297337@gmail.com>
2026-06-09 21:26:54 +0900

    dkanrjsk

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times: test edit'),
+            const Text('안녕하세요, 지은이에오'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
